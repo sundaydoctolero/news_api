0ef329e8d880a452bf887e90b35f16ec1ecc4b22
sysadmin29 <sysadmin@cccdms.com>
2018-08-23 06:41:55 +0800

    initial running backend code

---
+++ app/Http/routes.php
@@ -0,0 +1,14 @@
+<?php
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'api'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
